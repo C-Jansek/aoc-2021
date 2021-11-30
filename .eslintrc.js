f8d60a6dd513b5fcdb30105d5429c385775bbe16
C-Jansek <49492561+C-Jansek@users.noreply.github.com>
2021-11-30 19:42:03 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    plugins: ['sonarjs', 'unicorn'],
+    extends: ['google', 'plugin:sonarjs/recommended', 'plugin:unicorn/recommended'],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    env: {
+        node: true,
+        es6: true,
+    },
+    rules: {
+        indent: ['error', 4],
+        'max-len': [
+            'error',
+            100,
+            {
+                ignoreTemplateLiterals: true,
+            },
+        ],
+        'object-curly-spacing': ['error', 'always'],
+        'linebreak-style': 'off',
+        'brace-style': ['error', 'stroustrup'],
+        'operator-linebreak': ['error', 'before'],
+        'require-jsdoc': [
+            'error',
+            {
+                require: {
+                    ClassDeclaration: false,
+                },
+            },
+        ],
+        'unicorn/no-null': 'off',
+        'unicorn/prefer-module': 'off',
+        'no-undefined': 'error',
+        'unicorn/prefer-default-parameters': 'off',
+    },
+    overrides: [
+        {
+            files: ['package.json', 'package-lock.json'],
+            rules: {
+                indent: ['error', 2],
+            },
+        },
+    ],
+};
