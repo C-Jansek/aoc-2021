b2a31f90348aff5346e4d4eca7856bfcf8868804
C-Jansek <49492561+C-Jansek@users.noreply.github.com>
2021-12-06 07:37:37 +0100

    Refactor fish simulation

--- challenges/2021/6/index.ts
+++ challenges/2021/6/index.ts
@@ -1,54 +1,47 @@
 import getInput from '../../../utils/getInput';
 import * as _ from 'lodash';
 
-const part1 = () => {
-    const input = getInput('2021', '6')
-        .split('\n')[0]
-        .split(',')
-        .map((fish) => Number(fish));
+/**
+ * Return a new array of zeros (0) of length `length`
+ * @param {number} length
+ * @return {number[]}
+ */
+const newZerosArray = (length: number): number[] => Array.from({ length: length }).map(() => 0);
 
-    let lanternfish = input;
-
-    for (let day = 0; day < 80; day++) {
-        const newLanternfish: number[] = [];
-        lanternfish = lanternfish.map((fish) => {
-            if (fish === 0) {
-                newLanternfish.push(8);
-                return 6;
-            }
-            return fish - 1;
-        });
-        lanternfish.push(...newLanternfish);
-    }
-
-    return lanternfish.length;
-};
-
-const part2 = () => {
-    const input = getInput('2021', '6')
-        .split('\n')[0]
-        .split(',')
-        .map((fish) => Number(fish));
-
-    const days = 256;
-    const newFishAge = 8;
-    const reproducedFishAge = 6;
-
+/**
+ * Simulate fish population.
+ * For `days` days, fish will become one day closer to reproduction every day.
+ * Fish that reproduced will not reproduce the next `reproducedFishAge` days.
+ * Fish that are newly born will not reproduce the next `newlyBornAge` days.
+ *
+ * @param {number[]} input Days till reproduction of starting fish
+ * @param {number} [days = 256]
+ * @param {number} [newlyBornAge = 8]
+ * @param {number} [reproducedFishAge = 6]
+ * @return {number} Total fish after `days` days
+ */
+function simulateFishPopulation(
+    input: number[],
+    days = 256,
+    newlyBornAge = 8,
+    reproducedFishAge = 6,
+) {
     // Set starting fish days till reproduce
-    let fishDaysTillReproduce = newZerosArray(newFishAge + 1);
+    let fishDaysTillReproduce = newZerosArray(newlyBornAge + 1);
     for (const fish of input) {
         fishDaysTillReproduce[fish + 1] += 1;
     }
 
     // For each day
     for (let day = 0; day <= days; day++) {
-        const newFish = newZerosArray(newFishAge + 1);
-        for (let fishAge = newFishAge; fishAge >= 0; fishAge--) {
+        const newFish = newZerosArray(newlyBornAge + 1);
+        for (let fishAge = newlyBornAge; fishAge >= 0; fishAge--) {
             // Fishes at 0 reproduce
             if (fishAge === 0) {
                 newFish[reproducedFishAge] += fishDaysTillReproduce[0];
-                newFish[newFishAge] += fishDaysTillReproduce[0];
+                newFish[newlyBornAge] += fishDaysTillReproduce[0];
             }
+
             // Others get closer to reproduction
             else {
                 newFish[fishAge - 1] = fishDaysTillReproduce[fishAge];
@@ -58,9 +51,25 @@ const part2 = () => {
     }
     // Return total amount of fish
     return fishDaysTillReproduce.reduce((total, daysToGo) => total + daysToGo, 0);
+}
+
+const part1 = () => {
+    const input = getInput('2021', '6')
+        .split('\n')[0]
+        .split(',')
+        .map((fish) => Number(fish));
+
+    return simulateFishPopulation(input, 80);
 };
 
-const newZerosArray = (length: number): number[] => Array.from({ length: length }).map(() => 0);
+const part2 = () => {
+    const input = getInput('2021', '6')
+        .split('\n')[0]
+        .split(',')
+        .map((fish) => Number(fish));
+
+    return simulateFishPopulation(input, 256);
+};
 
 console.log(`Solution 1: ${part1()}`);
 console.log(`Solution 2: ${part2()}`);
