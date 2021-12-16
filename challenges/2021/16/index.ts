4696fdbfb9ce9bd8becd965e96dcf6bc1542b8d1
C-Jansek <49492561+C-Jansek@users.noreply.github.com>
2021-12-16 08:03:16 +0100

    Day 16 Part 2

--- challenges/2021/16/index.ts
+++ challenges/2021/16/index.ts
@@ -2,11 +2,11 @@ import { assert } from 'console';
 import getInput from '../../../utils/getInput';
 
 type Packet = {
-    version: string;
-    typeId: string;
+    version: number;
+    typeId: number;
     length: number;
     versionSum: number;
-    literalValue?: number;
+    value: number;
     subPackets?: (Packet | null)[];
 };
 
@@ -15,20 +15,16 @@ const parseHexadecimal = (input: string): string => {
     for (const char of input) {
         output += ('0000' + Number.parseInt(char, 16).toString(2)).slice(-4);
     }
-    // console.log(output);
     return output;
 };
 
 const unpack = (input: string): Packet | null => {
     if (Number.parseInt(input) === 0) return null;
-    // console.log(`------unpack\n${input}`);
-    const version = input.slice(0, 3);
-    const typeId = input.slice(3, 6);
+    const version = Number.parseInt(input.slice(0, 3), 2);
+    const typeId = Number.parseInt(input.slice(3, 6), 2);
+    const versionAndTypeLength = 6;
 
-    if (Number.parseInt(typeId, 2) === 4) {
-        // console.log({ version, typeId });
-        // console.log('\t\tV');
-
+    if (typeId === 4) {
         const valueBits = input.slice(6);
         const valuesArray: string[] = [];
         for (let index = 0; index - 1 < valueBits.length / 5; index++) {
@@ -38,35 +34,27 @@ const unpack = (input: string): Packet | null => {
             }
         }
 
-        const length =
-            valuesArray.join('').length + valuesArray.length + version.length + typeId.length;
-        // console.log(valuesArray.join('').length, version.length, typeId.length);
-        // console.log({
-        //     version,
-        //     typeId,
-        //     length,
-        //     literalValue: Number.parseInt(valuesArray.join(''), 2),
-        //     versionSum: Number.parseInt(version, 2),
-        // });
+        const length = valuesArray.join('').length + valuesArray.length + versionAndTypeLength;
         return {
             version,
             typeId,
             length,
-            literalValue: Number.parseInt(valuesArray.join(''), 2),
-            versionSum: Number.parseInt(version, 2),
+            value: Number.parseInt(valuesArray.join(''), 2),
+            versionSum: version,
         };
     }
 
     const dataBits = input.slice(7);
     const lengthTypeId = input[6];
 
+    const subPackets: Packet[] = [];
+    let versionSum = 0;
+    let length = 0;
     if (lengthTypeId === '0') {
         const lengthTypeIdLength = 15;
         const subPacketsTotalLength = Number.parseInt(dataBits.slice(0, lengthTypeIdLength), 2);
-        // console.log({ version, typeId, lengthTypeId, subPacketsTotalLength });
 
         const subPacketBits = dataBits.slice(lengthTypeIdLength);
-        const subPackets: Packet[] = [];
 
         // Split subpackets
         let index = 0;
@@ -76,35 +64,17 @@ const unpack = (input: string): Packet | null => {
             subPackets.push(subPacket);
             index += subPacket.length;
         }
-        const versionSum = subPackets.reduce(
-            (sum, current) => (sum += current.versionSum),
-            Number.parseInt(version, 2),
-        );
+        versionSum = subPackets.reduce((sum, current) => (sum += current.versionSum), version);
 
-        const length =
-            index + version.length + typeId.length + lengthTypeId.length + lengthTypeIdLength;
-
-        return {
-            version,
-            typeId,
-            length,
-            subPackets,
-            versionSum,
-        };
+        length = index + versionAndTypeLength + lengthTypeId.length + lengthTypeIdLength;
     } else {
         assert(lengthTypeId === '1');
         if (lengthTypeId !== '1') throw new Error('Not specified lengthTypId');
 
         const lengthTypeIdLength = 11;
         const subPacketCount = Number.parseInt(dataBits.slice(0, lengthTypeIdLength), 2);
-        // console.log({ version, typeId, lengthTypeId, subPacketCount });
 
         const subPacketBits = dataBits.slice(lengthTypeIdLength);
-        const subPackets: Packet[] = [];
-
-        // while (subPacketBits.slice(-subPacketCount) === '0'.repeat(subPacketCount)) {
-        //     subPacketBits = subPacketBits.slice(0, -subPacketCount);
-        // }
 
         // Split subpackets
         let index = 0;
@@ -118,51 +88,66 @@ const unpack = (input: string): Packet | null => {
         }
 
         // Unpack subPackets
-        const versionSum = subPackets.reduce(
-            (sum, current) => (sum += current.versionSum),
-            Number.parseInt(version, 2),
-        );
-        const length =
-            index + version.length + typeId.length + lengthTypeId.length + lengthTypeIdLength;
-
-        return {
-            version,
-            typeId,
-            length,
-            subPackets,
-            versionSum,
-        };
+        versionSum = subPackets.reduce((sum, current) => (sum += current.versionSum), version);
+        length = index + versionAndTypeLength + lengthTypeId.length + lengthTypeIdLength;
     }
+    let value = 0;
+
+    if (typeId === 0) {
+        value = subPackets.reduce((sum, current) => (sum += current.value), 0);
+    }
+    if (typeId === 1) {
+        value = subPackets.reduce((product, current) => (product *= current.value), 1);
+    }
+    if (typeId === 2) {
+        value = subPackets.reduce(
+            (min, current) => Math.min(min, current.value),
+            Number.POSITIVE_INFINITY,
+        );
+    }
+    if (typeId === 3) {
+        value = subPackets.reduce(
+            (max, current) => Math.max(max, current.value),
+            Number.NEGATIVE_INFINITY,
+        );
+    }
+    if (typeId === 5) {
+        value = subPackets[0].value > subPackets[1].value ? 1 : 0;
+    }
+    if (typeId === 6) {
+        value = subPackets[0].value < subPackets[1].value ? 1 : 0;
+    }
+    if (typeId === 7) {
+        value = subPackets[0].value === subPackets[1].value ? 1 : 0;
+    }
+
+    return {
+        version,
+        typeId,
+        length,
+        subPackets,
+        versionSum,
+        value,
+    };
 };
 
 const part1 = () => {
     const input = getInput('2021', '16').split('\n')[0];
-    // const input = 'D2FE28';
-    // const input = 'A0016C880162017C3686B18A3D4780';
 
     const binaryData = parseHexadecimal(input);
     const packet = unpack(binaryData);
-    // console.log(packet);
-
-    // const input2 = '38006F45291200';
-
-    // const binaryData2 = parseHexadecimal(input2);
-    // const packets2 = unpack(binaryData2);
-    // console.log(packets2);
-
-    // const input3 = '8A004A801A8002F478';
-
-    // const binaryData3 = parseHexadecimal(input3);
-    // const packets3 = unpack(binaryData3);
-    // console.log(packets3);
 
     return packet?.versionSum ?? -1;
 };
 
 const part2 = () => {
-    const input = getInput('2021', '16').split('\n');
+    const input = getInput('2021', '16').split('\n')[0];
 
-    return;
+    const binaryData = parseHexadecimal(input);
+    const packet = unpack(binaryData);
+    console.log(packet);
+
+    return packet?.value ?? -1;
 };
 
 console.log(`Solution 1: ${part1()}`);
