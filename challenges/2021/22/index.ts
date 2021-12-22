43bbe5a30609436f4dcadba79bde1eae3dd266c0
C-Jansek <49492561+C-Jansek@users.noreply.github.com>
2021-12-22 08:57:34 +0100

    Well this runs into memory things

--- challenges/2021/22/index.ts
+++ challenges/2021/22/index.ts
@@ -1,6 +1,6 @@
 import { assert } from 'console';
 import { inRange } from 'lodash';
-import { Vector3 } from 'three';
+import { Box3, InstancedInterleavedBuffer, Vector3 } from 'three';
 import getInput from '../../../utils/getInput';
 
 type Point = {
@@ -14,6 +14,11 @@ type Instruction = {
     state: boolean;
     start: Vector3;
     end: Vector3;
+};
+type InstructionId = {
+    state: boolean;
+    box: Box3;
+    id: number;
 };
 
 const part1 = () => {
@@ -111,7 +116,8 @@ const part2 = () => {
         .split('\n')
         .filter((line) => line !== '');
 
-    const instructions: Instruction[] = [];
+    const instructions: InstructionId[] = [];
+    let id = 0;
     for (const instruction of input) {
         const state = !!instruction.match('on');
         const instrs = instruction.match(/(?!=)-*\d+\.\.-*\d+/g);
@@ -119,101 +125,192 @@ const part2 = () => {
 
         const coords = instrs.map((inst) => inst.split('..').map((value) => Number(value)));
         const start = new Vector3(...coords?.map((value) => value[0]));
-        const end = new Vector3(...coords?.map((value) => value[1]));
+        const end = new Vector3(...coords?.map((value) => value[1] + 1));
+        const box = new Box3(start, end);
         instructions.push({
+            id,
             state,
-            start,
-            end,
+            box,
         });
+        id++;
     }
 
-    const turnedOn: Instruction[] = [];
-    const turnedOff: Instruction[] = [];
+    let turnedOn: InstructionId[] = [];
 
+    let instructionIndex = 0;
     for (const instruction of instructions) {
-        console.log('handle', instruction);
+        console.log(instructionIndex, turnedOn.length);
         if (instruction.state) {
-            let overlapTrueCount = 0;
-            let overlapFalseCount = 0;
-            for (const allreadyOn of turnedOn) {
-                const overlapping = overlaps(instruction, allreadyOn, false);
-                // Overlapping double??
-                if (overlapping && overlapping !== true) {
-                    turnedOff.push(overlapping);
-                }
+            let nonOverlapping = [instruction];
+            for (const isOn of turnedOn) {
+                nonOverlapping = checkOverlapping(isOn, nonOverlapping, true);
+                if (nonOverlapping.length === 0) break;
             }
-
-            turnedOn.push(instruction);
+            if (nonOverlapping.length > 0) turnedOn.unshift(...nonOverlapping);
         } else {
-            for (const allreadyOn of turnedOn) {
-                const overlapping = overlaps(instruction, allreadyOn, false);
-                // Overlapping double??
-                if (overlapping && overlapping !== true) {
-                    turnedOff.push(overlapping);
-                    break;
-                }
-            }
+            turnedOn = checkOverlapping(instruction, turnedOn, true);
         }
+        instructionIndex++;
     }
 
     let totalOn = 0;
     for (const instruction of turnedOn) {
-        totalOn += Math.abs(
-            (instruction.end.x - instruction.start.x + 1) *
-                (instruction.end.y - instruction.start.y + 1) *
-                (instruction.end.z - instruction.start.z + 1),
-        );
-        console.log(
-            instruction,
-            (instruction.end.x - instruction.start.x + 1) *
-                (instruction.end.y - instruction.start.y + 1) *
-                (instruction.end.z - instruction.start.z + 1),
-        );
+        totalOn += spaceSize(instruction.box.min, instruction.box.max);
     }
 
-    console.log('off ---------------------');
-    let totalOff = 0;
-    for (const instruction of turnedOff) {
-        totalOff += Math.abs(
-            (instruction.end.x - instruction.start.x + 1) *
-                (instruction.end.y - instruction.start.y + 1) *
-                (instruction.end.z - instruction.start.z + 1),
-        );
-        console.log(
-            instruction,
-            (instruction.end.x - instruction.start.x + 1) *
-                (instruction.end.y - instruction.start.y + 1) *
-                (instruction.end.z - instruction.start.z + 1),
-        );
-    }
+    console.log(turnedOn);
+    return totalOn;
+    // for (const instruction of instructions) {
+    //     // console.log('handle', instruction);
+    //     if (instruction.state) {
+    //         for (const allreadyOn of turnedOn) {
+    //             const overlapping = overlaps(instruction, allreadyOn, false);
+    //             // Overlapping double??
+    //             if (overlapping && overlapping !== true) {
+    //                 // const overlapTrueCount = turnedOn
+    //                 //     .map((inst) => !!overlaps(inst, overlapping, false))
+    //                 //     .reduce((total, current) => (current ? total + 1 : total), 0);
+    //                 // const overlapFalseCount = turnedOff
+    //                 //     .map((inst) => !!overlaps(inst, overlapping, false))
+    //                 //     .reduce((total, current) => (current ? total + 1 : total), 0);
 
-    console.log('on', totalOn);
-    console.log('Off', totalOff);
+    //                 // if (overlapTrueCount > overlapFalseCount) {
+    //                 turnedOff.push(overlapping);
+    //                 // }
+    //             }
+    //         }
 
-    return totalOn - totalOff;
+    //         turnedOn.push(instruction);
+    //     } else {
+    //         for (const allreadyOn of turnedOn) {
+    //             const overlapping = overlaps(instruction, allreadyOn, false);
+    //             // Overlapping double??
+    //             if (overlapping && overlapping !== true) {
+    //                 turnedOff.push(overlapping);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // let totalOn = 0;
+    // for (const instruction of turnedOn) {
+    //     totalOn += Math.abs(
+    //         (instruction.end.x - instruction.start.x + 1) *
+    //             (instruction.end.y - instruction.start.y + 1) *
+    //             (instruction.end.z - instruction.start.z + 1),
+    //     );
+    //     // console.log(
+    //     //     instruction,
+    //     //     (instruction.end.x - instruction.start.x + 1) *
+    //     //         (instruction.end.y - instruction.start.y + 1) *
+    //     //         (instruction.end.z - instruction.start.z + 1),
+    //     // );
+    // }
+
+    // // console.log('off ---------------------');
+    // let totalOff = 0;
+    // for (const instruction of turnedOff) {
+    //     totalOff += Math.abs(
+    //         (instruction.end.x - instruction.start.x + 1) *
+    //             (instruction.end.y - instruction.start.y + 1) *
+    //             (instruction.end.z - instruction.start.z + 1),
+    //     );
+    //     // console.log(
+    //     //     instruction,
+    //     //     (instruction.end.x - instruction.start.x + 1) *
+    //     //         (instruction.end.y - instruction.start.y + 1) *
+    //     //         (instruction.end.z - instruction.start.z + 1),
+    //     // );
+    // }
+
+    // console.log('on', totalOn);
+    // console.log('Off', totalOff);
+
+    // return totalOn - totalOff;
 };
 
-const overlaps = (a: Instruction, b: Instruction, state: boolean): Instruction | boolean => {
-    if (smallerAThanB(b.start, a.start) && smallerAThanB(a.start, b.end)) {
-        const start = a.start;
-        const end = a.end.clone().clamp(b.start, b.end);
-        return {
-            state,
-            start,
-            end,
-        };
-    }
-    if (smallerAThanB(b.start, a.end) && smallerAThanB(a.end, b.end)) {
-        const start = a.start.clone().clamp(b.start, b.end);
-        const end = a.end;
-        return {
-            state,
-            start,
-            end,
-        };
-    }
+const spaceSize = (a: Vector3, b: Vector3) => {
+    return Math.abs((b.x - a.x) * (b.y - a.y) * (b.z - a.z));
+};
 
-    return false;
+const checkOverlapping = (
+    allreadyOn: InstructionId,
+    turnsOn: InstructionId[],
+    state: boolean,
+): InstructionId[] => {
+    // console.log('allreadyOn', allreadyOn.box);
+    // console.log(
+    //     'turnsOn',
+    //     turnsOn.map((inst) => inst.box),
+    // );
+    const nonOverlapping: InstructionId[] = [];
+
+    let toIndex = 0;
+    console.log({ toIndex }, turnsOn.length);
+    for (const turnOn of turnsOn) {
+        // if (toIndex % 10000 === 0) console.log({ toIndex });
+        if (!turnOn.box.intersectsBox(allreadyOn.box)) {
+            nonOverlapping.push(turnOn);
+            continue;
+        }
+
+        const intersection = turnOn.box.clone().intersect(allreadyOn.box);
+
+        const xValues = new Set<number>();
+        const yValues = new Set<number>();
+        const zValues = new Set<number>();
+
+        xValues.add(turnOn.box.min.x);
+        xValues.add(turnOn.box.max.x);
+        xValues.add(intersection.min.x);
+        xValues.add(intersection.max.x);
+
+        yValues.add(turnOn.box.min.y);
+        yValues.add(turnOn.box.max.y);
+        yValues.add(intersection.min.y);
+        yValues.add(intersection.max.y);
+
+        zValues.add(turnOn.box.min.z);
+        zValues.add(turnOn.box.max.z);
+        zValues.add(intersection.min.z);
+        zValues.add(intersection.max.z);
+
+        const xValuesSorted = [...xValues].sort();
+        const yValuesSorted = [...yValues].sort();
+        const zValuesSorted = [...zValues].sort();
+
+        for (let xIndex = 0; xIndex < xValues.size - 1; xIndex++) {
+            for (let yIndex = 0; yIndex < yValues.size - 1; yIndex++) {
+                for (let zIndex = 0; zIndex < zValues.size - 1; zIndex++) {
+                    const start = new Vector3(
+                        xValuesSorted[xIndex],
+                        yValuesSorted[yIndex],
+                        zValuesSorted[zIndex],
+                    );
+                    const end = new Vector3(
+                        xValuesSorted[xIndex + 1],
+                        yValuesSorted[yIndex + 1],
+                        zValuesSorted[zIndex + 1],
+                    );
+                    const box = new Box3(start, end);
+                    if (
+                        (!box.min.equals(intersection.min) || !box.max.equals(intersection.max)) &&
+                        box.intersectsBox(turnOn.box)
+                    ) {
+                        nonOverlapping.push({
+                            id: turnOn.id,
+                            state,
+                            box,
+                        });
+                    }
+                }
+            }
+        }
+        toIndex++;
+    }
+    // console.log(nonOverlapping.map((no) => no.box));
+    return nonOverlapping;
 };
 
 const smallerAThanB = (a: Vector3, b: Vector3) => {
