12fc8b597adb533d6d2d65587e818f4ad36ace6b
C-Jansek <49492561+C-Jansek@users.noreply.github.com>
2021-12-23 11:40:25 +0100

    Minor refactoring

--- challenges/2021/19/index.ts
+++ challenges/2021/19/index.ts
@@ -22,29 +22,30 @@ import getInput from '../../../utils/getInput';
 // ];
 
 const generatePossibleOrientations = () => {
-    const possibilities: Matrix3[] = [];
-    const turnMatrix = new Matrix3().set(0, 1, 0, -1, 0, 0, 0, 0, 1);
-    const turn = (m: Matrix3) => m.multiply(turnMatrix);
+    const orientations: Matrix3[] = [];
+    const zRollMatrix = new Matrix3().set(0, 1, 0, -1, 0, 0, 0, 0, 1);
+    const rollZ = (m: Matrix3) => m.multiply(zRollMatrix);
 
-    const rollFrontMatrix = new Matrix3().set(1, 0, 0, 0, 0, -1, 0, 1, 0);
-    const rollFront = (m: Matrix3) => m.multiply(rollFrontMatrix);
+    const yRollMatrix = new Matrix3().set(1, 0, 0, 0, 0, -1, 0, 1, 0);
+    const rollY = (m: Matrix3) => m.multiply(yRollMatrix);
 
-    const rollLeftMatrix = new Matrix3().set(0, 0, 1, 0, 1, 0, -1, 0, 0);
-    const rollLeft = (m: Matrix3) => m.multiply(rollLeftMatrix);
+    const xRollMatrix = new Matrix3().set(0, 0, 1, 0, 1, 0, -1, 0, 0);
+    const rollX = (m: Matrix3) => m.multiply(xRollMatrix);
 
     const matrix = new Matrix3();
-    for (let index = 0; index < 3; index++) {
-        rollFront(matrix);
+    for (let _ = 0; _ < 3; _++) {
+        rollY(matrix);
         for (let _index = 0; _index < 4; _index++) {
-            possibilities.push(new Matrix3().multiply(turn(matrix)));
+            orientations.push(new Matrix3().multiply(rollZ(matrix)));
         }
 
-        rollLeft(matrix);
+        rollX(matrix);
         for (let _index = 0; _index < 4; _index++) {
-            possibilities.push(new Matrix3().multiply(turn(matrix)));
+            orientations.push(new Matrix3().multiply(rollZ(matrix)));
         }
     }
-    return possibilities;
+
+    return orientations;
 };
 
 class Scanner {
@@ -70,36 +71,44 @@ class Scanner {
         return report;
     }
 
-    relativePositionFromReport(report: Vector3[]): Vector3 | false {
-        for (const baseA of this.report) {
-            for (const baseB of report) {
-                const mapping: { [key: number]: number } = {};
-                const relativePostion = new Vector3().subVectors(baseA, baseB);
-                // console.log('\n\n -----------------\n', baseA, baseB, relativePostion);
+    // relativePositionWithRotation(otherScanner: Scanner, orientation: Matrix3): Vector3 | false {
+    //     const otherReport = otherScanner.getRotatedReport(orientation);
+    //     // this.report vs otherReport;
+    // }
+
+    relativePositionFromReport(otherReport: Vector3[]): Vector3 | null {
+        for (const _beaconA of this.report) {
+            for (const _beaconB of otherReport) {
+                // const overlappingBeacons = this.findOverlappingBeacons(this.report, otherReport);
+
+                const mapping: Record<number, number> = {};
+                const positionDelta = new Vector3().subVectors(_beaconA, _beaconB);
+                // console.log('\n\n -----------------\n', _beaconA, _beaconB, positionDelta);
 
                 for (const [indexA, beaconA] of this.report.entries()) {
-                    if (beaconA === baseA) continue;
+                    if (beaconA === _beaconA) continue;
 
-                    for (const [indexB, beaconB] of report.entries()) {
-                        if (beaconB === baseB) continue;
-                        if (Object.values(mapping).includes(indexB)) continue;
+                    for (const [indexB, beaconB] of otherReport.entries()) {
+                        if (beaconB === _beaconB || Object.values(mapping).includes(indexB)) {
+                            continue;
+                        }
 
                         if (
                             beaconB
                                 .clone()
-                                .add(relativePostion)
+                                .add(positionDelta)
                                 .equals(beaconA)
                         ) {
                             mapping[indexA] = indexB;
                         }
                     }
                 }
-                if (Object.values(mapping).length >= 12 - 1) {
-                    return relativePostion;
+                if (Object.values(mapping).length > 12) {
+                    return positionDelta;
                 }
             }
         }
-        return false;
+        return null;
     }
 
     getRotatedReport(orientation: Matrix3): Vector3[] {
@@ -118,53 +127,58 @@ class Scanner {
 }
 
 const part1 = () => {
-    // const scannersInput = getInput('2021', '19').split('\n\n');
-    // const scanners: Scanner[] = [];
-    // for (const scanner of scannersInput) {
-    //     const id = Number(scanner.split('\n')[0].match(/\d+/g));
-    //     const report = scanner.split('\n').slice(1);
-    //     scanners.push(new Scanner(id, report, id === 0 ? true : false, id === 0 ? true : false));
-    // }
-    // while (!scanners.every((scanner) => scanner.positionKnown)) {
-    //     for (const scannerA of scanners) {
-    //         if (!scannerA.positionKnown) continue;
-    //         for (const scannerB of scanners) {
-    //             // No need to match if already found
-    //             if (scannerB.positionKnown) continue;
-    //             if (scannerA === scannerB) continue;
-    //             const possibleOrientations = generatePossibleOrientations();
-    //             for (const orientation of possibleOrientations) {
-    //                 const beaconsB = scannerB.getRotatedReport(orientation);
-    //                 const relativePosition = scannerA.relativePositionFromReport(beaconsB);
-    //                 if (relativePosition) {
-    //                     console.log(scannerA.id, scannerB.id);
-    //                     // console.log(scannerA.position);
-    //                     // console.log(relativePosition);
-    //                     scannerB.setPosition(relativePosition, orientation);
-    //                     break;
-    //                 }
-    //                 // break; // testing
-    //             }
-    //             // break; // testing
-    //         }
-    //         // break; // testing
-    //     }
-    // }
-    // console.log(scanners);
-    // // console.log(scanners.map((scanner) => _.orderBy(scanner.report, ['x', 'y', 'z'], 'asc')));
-    // const allBeacons: Vector3[] = [];
-    // for (const scanner of scanners) {
-    //     for (const beacon of scanner.report) {
-    //         allBeacons.push(beacon);
-    //     }
-    // }
-    // const uniqBeacons = _.uniqBy(allBeacons, (a: Vector3) => hash(a));
-    // const sortedBeacons = _.orderBy(uniqBeacons, ['x', 'y', 'z'], 'asc');
-    // // console.log(sortedBeacons);
-    // return sortedBeacons.length;
+    const scannersInput = getInput('2021', '19').split('\n\n');
+    const scanners: Scanner[] = [];
+
+    for (const scanner of scannersInput) {
+        const id = Number(scanner.split('\n')[0].match(/\d+/g));
+        const report = scanner.split('\n').slice(1);
+        scanners.push(new Scanner(id, report, id === 0 ? true : false, id === 0 ? true : false));
+    }
+
+    const possibleOrientations = generatePossibleOrientations();
+
+    while (!scanners.every((scanner) => scanner.positionKnown)) {
+        for (const scannerA of scanners) {
+            if (!scannerA.positionKnown) continue;
+            for (const scannerB of scanners) {
+                // No need to match if already found
+                if (scannerB.positionKnown || scannerA === scannerB) {
+                    continue;
+                }
+
+                for (const orientation of possibleOrientations) {
+                    const positionDelta = scannerA.relativePositionFromReport(
+                        scannerB.getRotatedReport(orientation),
+                    );
+                    if (!positionDelta) continue;
+
+                    console.log(scannerA.id, scannerB.id);
+                    // console.log(scannerA.position);
+                    // console.log(positionDelta);
+                    scannerB.setPosition(positionDelta, orientation);
+                    break;
+                }
+                // break; // testing
+            }
+            // break; // testing
+        }
+    }
+    console.log(scanners);
+    // console.log(scanners.map((scanner) => _.orderBy(scanner.report, ['x', 'y', 'z'], 'asc')));
+    const allBeacons: Vector3[] = [];
+    for (const scanner of scanners) {
+        for (const beacon of scanner.report) {
+            allBeacons.push(beacon);
+        }
+    }
+    const uniqBeacons = _.uniqBy(allBeacons, (a: Vector3) => hash(a));
+    const sortedBeacons = _.orderBy(uniqBeacons, ['x', 'y', 'z'], 'asc');
+    // console.log(sortedBeacons);
+    return sortedBeacons.length;
 };
 
-const hash = (v: Vector3) => `${v.x},${v.y},${v.z}`;
+const hash = (v: Vector3) => v.toArray().join('-');
 
 const part2 = () => {
     const scannersInput = getInput('2021', '19').split('\n\n');
@@ -174,18 +188,19 @@ const part2 = () => {
         const id = Number(scanner.split('\n')[0].match(/\d+/g));
         const report = scanner.split('\n').slice(1);
 
-        scanners.push(new Scanner(id, report, id === 0 ? true : false, id === 0 ? true : false));
+        scanners.push(new Scanner(id, report, id === 0, id === 0));
     }
+
+    const possibleOrientations = generatePossibleOrientations();
+
     while (!scanners.every((scanner) => scanner.positionKnown)) {
         for (const scannerA of scanners) {
             if (!scannerA.positionKnown) continue;
+
             for (const scannerB of scanners) {
                 // No need to match if already found
-                if (scannerB.positionKnown) continue;
+                if (scannerB.positionKnown || scannerA === scannerB) continue;
 
-                if (scannerA === scannerB) continue;
-
-                const possibleOrientations = generatePossibleOrientations();
                 for (const orientation of possibleOrientations) {
                     const beaconsB = scannerB.getRotatedReport(orientation);
 
